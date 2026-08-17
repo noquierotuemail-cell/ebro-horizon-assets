c51286b0ee517559014ad7c2aea9c3041b2a18dc
noquierotuemail-cell <noquierotuemail@gmail.com>
2026-08-17 22:21:46 +0200

    Bust HABRO slider cache

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
 (() => {
+  const VERSION = '20260817-2212';
   const frame = document.getElementById('interactive-device');
   if (frame) frame.id = 'interactive-device-core-paused';
 
   const loadSlider = () => {
     if (frame) frame.id = 'interactive-device';
     const slider = document.createElement('script');
-    slider.src = 'js/slider-v20260817.js?v=20260817-2204';
+    slider.src = `js/slider-v20260817.js?v=${VERSION}`;
     slider.defer = true;
     document.body.appendChild(slider);
   };
 
   const core = document.createElement('script');
-  core.src = 'js/app-core-v20260817.js?v=20260817-2204';
+  core.src = `js/app-core-v20260817.js?v=${VERSION}`;
   core.onload = loadSlider;
   core.onerror = loadSlider;
   document.head.appendChild(core);
