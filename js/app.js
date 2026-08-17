2b29c478f79c0570b8a89e3b6945a6e28ea6843a
noquierotuemail-cell <noquierotuemail@gmail.com>
2026-08-17 21:15:52 +0200

    Refresh slider runtime cache

--- js/app.js
+++ js/app.js
@@ -5,13 +5,13 @@
   const loadSlider = () => {
     if (frame) frame.id = 'interactive-device';
     const slider = document.createElement('script');
-    slider.src = 'js/slider-v20260817.js?v=20260817-2112';
+    slider.src = 'js/slider-v20260817.js?v=20260817-2118';
     slider.defer = true;
     document.body.appendChild(slider);
   };
 
   const core = document.createElement('script');
-  core.src = 'js/app-core-v20260817.js?v=20260817-2112';
+  core.src = 'js/app-core-v20260817.js?v=20260817-2118';
   core.onload = loadSlider;
   core.onerror = loadSlider;
   document.head.appendChild(core);
