4af358b246a09ea1c445af64eb518906f07738d9
noquierotuemail-cell <noquierotuemail@gmail.com>
2026-08-18 08:00:22 +0200

    Add cache-busted HABRO app bootstrap

---
+++ js/app-v20260818-0748.js
@@ -0,0 +1,18 @@
+(() => {
+  const frame = document.getElementById('interactive-device');
+  if (frame) frame.id = 'interactive-device-core-paused';
+
+  const loadSlider = () => {
+    if (frame) frame.id = 'interactive-device';
+    const slider = document.createElement('script');
+    slider.src = 'js/slider-v20260817.js?v=20260818-0748';
+    slider.defer = true;
+    document.body.appendChild(slider);
+  };
+
+  const core = document.createElement('script');
+  core.src = 'js/app-core-v20260817.js?v=20260818-0748';
+  core.onload = loadSlider;
+  core.onerror = loadSlider;
+  document.head.appendChild(core);
+})();
